1b14330b6a62d59b456871e9d2c42642055f5e4e
hyrum james bullock <52971534+HyrumB@users.noreply.github.com>
2023-05-31 14:45:30 -0600

    Add files via upload

--- week05/student_files/scripts/task5.js
+++ week05/student_files/scripts/task5.js
@@ -59,30 +59,43 @@ document.getElementById("message2").textContent = messageTwo;
 
 /* FETCH */
 // Step 1: Declare a global empty array variable to store a list of temples
-const templeList = [];
+var templeList = [];
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
 
-function output(){
-    // - Creates an HTML <article> element
-    const newArticle = document.createElement("ARTICLE");
-    // - Creates an HTML <h3> element and add the temple's templeName property to it
-    const templeName = document.createElement("H3");
+function output(templeList){
+    
+    for (let index = 0; index < templeList.length; index++) {
+        var templeDict = templeList[index]
 
-    // - Creates an HTML <h4> element and add the temple's location property to it
-    const templeLocation = document.createElement("H4");
+        // - Creates an HTML <article> element
+        const newArticle = document.createElement("ARTICLE");
 
-    // - Creates an HTML <h4> element and add the temple's dedicated property to it
-    const templeDedicProp = document.createElement("H4");
+        // - Creates an HTML <h3> element and add the temple's templeName property to it
+        const templeName = document.createElement("H3");
+        templeName.textContent = templeDict["templeName"];
 
-    // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
-    const templeImg = document.createElement("IMG");
+        // - Creates an HTML <h4> element and add the temple's location property to it
+        const templeLocation = document.createElement("H4");
+        templeLocation.textContent = templeDict["location"];
 
-    // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
-    newArticle.appendChild(templeName);
-    newArticle.appendChild(templeLocation);
-    newArticle.appendChild(templeDedicProp);
-    newArticle.appendChild(templeImg);
-    // - Appends the <article> element to the HTML element with an ID of temples
+        // - Creates an HTML <h4> element and add the temple's dedicated property to it
+        const templeDedication = document.createElement("H4");
+        templeInfo.textContent = templeDict["dedicated"];
+
+        // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
+        const templeImg = document.createElement("IMG");
+        newArticle.textContent = templeDict["imageUrl"];
+
+        // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+        newArticle.appendChild(templeName);
+        newArticle.appendChild(templeLocation);
+        newArticle.appendChild(templeDedication);
+        newArticle.appendChild(templeImg);
+        
+        // - Appends the <article> element to the HTML element with an ID of temples
+        document.getElementById('temples').appendChild(newArticle);
+
+    }
     
 }
 // Step 3: Create another function called getTemples. Make it an async function.
@@ -91,19 +104,25 @@ async function getTemples(){
     //absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. 
     //Create a variable to hold the response from your fetch. You should have the 
     //program wait on this line until it finishes.
-    var templeInfo = await fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json");
-
-    // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). 
-    //Store this in the templeList variable you declared earlier (Step 1). 
-    //Make sure the the execution of the code waits here as well until it finishes.
-    templeList.append(JSON.parse(templeInfo)) 
-
-
-    // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+    var response = await fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json");
+    if(response.ok){
+  
+        // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). 
+        //Store this in the templeList variable you declared earlier (Step 1). 
+        //Make sure the the execution of the code waits here as well until it finishes.
+        //console.log(templeInfo.json)
+        templeList = response.json() 
+        console.log(templeList)
+        // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+        output(templeList)
+    }
 }
+
 getTemples()
 // Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
-
+function reset() {
+    
+}
 // Step 8: Declare a function named sortBy that does the following:
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
